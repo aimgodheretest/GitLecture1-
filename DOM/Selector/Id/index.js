afb3c25c6ffef9c154f4d62818c4c1baf00af406
Murli Kumar <aimgodhere@gmail.com>
2026-01-30 18:59:59 +0530

    task done for id selector

---
+++ DOM/Selector/Id/index.js
@@ -0,0 +1,14 @@
+const heading = document.getElementById("main-heading");
+heading.textContent = "Fruit World";
+heading.style.color = "orange";
+
+const header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.borderBottom = "1px solid orange";
+
+const basketHeading = document.getElementById("basket-heading");
+
+basketHeading.style.color = "green";
+
+const regards = document.getElementById("thanks");
+regards.innerHTML = "<p>Please visit us again</p>";
