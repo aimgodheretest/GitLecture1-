f56d84d98a38462c28a007fcc5320a4f1a6e2400
Murli Kumar <aimgodhere@gmail.com>
2026-02-05 16:52:38 +0530

    full functionality added

--- JavascriptProject1/app.js
+++ JavascriptProject1/app.js
@@ -6,23 +6,76 @@ const addBtn = document.getElementById("addBtn");
 const expenseList = document.getElementById("expenseList");
 
 //get expenses from the localStorage//
-function getExpenses(){
-    return JSON.parse(localStorage.getItem('expenses',)) || [];
+function getExpenses() {
+  return JSON.parse(localStorage.getItem("expenses")) || [];
 }
 //save expenses to the localStorage//
-function saveExpenses(expenses){
-    localStorage.saveItem('expenses',JSON.stringify(expenses))
+function saveExpenses(expenses) {
+  localStorage.setItem("expenses", JSON.stringify(expenses));
 }
 
-addBtn.addEventListener('click',function(){
-    const amount = amountInput.value;
-    const description = descInput.value;
-    const category = categoryInput.value;
+// add expense
+addBtn.addEventListener("click", function () {
+  const amount = amountInput.value;
+  const description = descInput.value;
+  const category = categoryInput.value;
 
-    if(!amount || !description){
-        alert('Please fill all fields')
-        return;
-    }
+  if (!amount || !description) {
+    alert("Please fill all fields");
+    return;
+  }
 
-    const expenses = getExpenses()
-})
+  const expenses = getExpenses();
+  expenses.push({ amount, description, category });
+  saveExpenses(expenses);
+  renderExpenses();
+  clearInputs();
+});
+
+//delete expense//
+function deleteExpense(index) {
+  const expenses = getExpenses();
+  expenses.splice(index, 1);
+  saveExpenses(expenses);
+  renderExpenses();
+}
+
+//edit expense//
+function editExpense(index) {
+  const expenses = getExpenses();
+  const expense = expenses[index];
+
+  amountInput.value = expense.amount;
+  descInput.value = expense.description;
+  categoryInput.value = expense.category;
+  deleteExpense(index);
+}
+
+//clear input fields//
+function clearInputs() {
+  amountInput.value = "";
+  descInput.value = "";
+  categoryInput.value = "";
+}
+// render expenses//
+function renderExpenses() {
+  const expenses = getExpenses();
+  expenseList.innerHTML = "";
+
+  expenses.forEach((expense, index) => {
+    const li = document.createElement("li");
+
+    //template literal to display expense details//
+
+    li.innerHTML = `
+      <strong>Amount:</strong> ${expense.amount} <br>
+      <strong>Description:</strong> ${expense.description} <br>
+      <strong>Category:</strong> ${expense.category} <br>
+      <button onclick="editExpense(${index})">Edit</button>
+      <button onclick="deleteExpense(${index})">Delete</button>
+    `;
+    expenseList.appendChild(li);
+  });
+}
+//page load render expenses//
+window.onload = renderExpenses;
